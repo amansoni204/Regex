80dc88a0b6b8fc3503a37a4d60c493563b2807c0
amansoni <amansoni2041999@gmail.com>
2023-01-30 23:45:49 +0530

    performing regex program

--- Program.cs
+++ Program.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace Regex
 {
-    internal class Program
+    internal class RegularExpressions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Regular Expressions Program");
             RegularExpressions regular = new RegularExpressions();
-            regular.FirstName();
-        }
+            Console.WriteLine("1.Enter the First Name \n2.Enter the Last Name");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    regular.FirstName();
+                    break;
+                case 2:
+                    regular.LastName();
+                    break;
+            }
     }
 }
