598803951cd8725375a68b23fbcf2d21c0c96318
amansoni <amansoni2041999@gmail.com>
2023-01-30 23:38:32 +0530

    performing regex program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegularExpressions regular = new RegularExpressions();
+            regular.FirstName();
+        }
+    }
+}
